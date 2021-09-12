e7711e8a6b9fda09c08b08776f5e1887ecf5f954
A.J.Sarmah <thisisamlan49@gmail.com>
2021-09-12 19:08:11 +0530

    Updated temp token

--- src/setup.js
+++ src/setup.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "9a8ccb5e03c541f89f3ec3d1daf09b69";
 const token =
-  "006e67979c34c1a44829ec2c144a542aed7IABg2yFKXu/Y5oub8dKH9Jd4/Mdp2JKB+3uDkaged4HMK2TNKL8AAAAAEADZzsCB7Rk/YQEAAQAUGj9h";
+  "006e67979c34c1a44829ec2c144a542aed7IACGKChEXbKvzE5ib0efPViWOsjZWxnXb/qa2f1vh/VezmTNKL8AAAAAEABgsZT+LFM/YQEAAQBTUz9h";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
